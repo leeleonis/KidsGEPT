39ab68ef14a011bd74ced64877570480486a0a1c
leonis <p6492leonis@gmail.com>
2015-06-18 09:49:01 +0800

    新增ViewModel

---
+++ KidsGEPT/Areas/backend/ViewModel/SchedulesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KidsGEPT.Models;
+
+namespace KidsGEPT.Models
+{
+    public  class SchedulesViewModel
+    {
+        public Basic BasicVM { get; set; }
+        public Schedule ScheduleVM { get; set; }
+    }
+}
